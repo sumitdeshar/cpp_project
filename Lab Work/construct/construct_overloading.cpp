e730b904eb3052df312264454f3e9bfabc5f4106
Sumit Deshar <sumeetdsr@gmail.com>
2021-06-18 14:54:45 +0545

    construct overloading

---
+++ Lab Work/construct/construct_overloading.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class construct
+{
+    private:
+            int a , b ;
+    public:
+            construct()
+            {
+                a=10;
+                b=20;
+            }
+            construct(int a1, int b1)
+            {
+                a=a1;
+                b=b1;
+            }
+            int getX()
+            {
+                return a;
+            }
+            int getY()
+            {
+                return b;
+            }
+
+};
+int main(){
+   construct c1(10,20),c;
+   cout<<"when no argument is passed : "<<endl;
+   cout<<"a="<<c.getX()<< "b="<<c.getY()<<endl;
+   cout<<"when (10,20) is passed."<<endl;
+   cout<<"a="<<c1.getX()<< "b="<<c1.getY()<<endl;
+}
